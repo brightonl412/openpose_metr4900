4567edd3fc50570d64c8ee6b12153673511aba0f
brightonl412 <brightonl412@gmail.com>
2020-05-05 16:38:27 +1000

    pyqt5 progress bar code not currently operational

---
+++ progress.py
@@ -0,0 +1,49 @@
+import sys
+from PyQt5.QtWidgets import QWidget, QProgressBar, QPushButton, QApplication
+from PyQt5 import QtCore, QtGui, QtWidgets
+import time
+
+
+class Frame_Counter():
+    def __init__(self, max_frames):
+        self.max_frames = max_frames
+        self.current_frame = 0
+        self.progress = 0
+
+    def incre_frame(self):
+        self.current_frame += 1
+        self.progress = (self.current_frame/self.max_frames)*100
+
+TIME_LIMIT = 2400000000000000000000000000
+class External(QtCore.QThread):
+    """
+    Runs a counter thread.
+    """
+    countChanged = QtCore.pyqtSignal(int)
+    def __init__(self, frame_counter):
+        super(External, self).__init__()
+        self.frame_counter = frame_counter
+
+    def run(self):
+        count = 0
+        while count < TIME_LIMIT:
+            count +=1
+            self.countChanged.emit(self.frame_counter.progress)
+            if self.frame_counter.progress == 100:
+                break
+
+
+def test3_func(progressUI):
+    a= Frame_Counter(100)
+    #print(a.max_frames)
+    #a.incre_frame()
+    #print(a.current_frame)
+    progressUI.start(External(a))
+    a.incre_frame()
+    a.incre_frame()
+
+if __name__=='__main__':
+	app = QApplication(sys.argv)
+
+
+	sys.exit(app.exec_())
